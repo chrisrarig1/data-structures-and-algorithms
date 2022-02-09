16e6589f852d5a71a9743239e728f8b6d76a3f2e
Chris Rarig <cwrarig20@gmail.com>
2022-02-09 00:14:38 -0600

    added test code for code challenge 17

--- python/code_challenges/tree_breadth_first/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first/tree_breadth_first.py
@@ -1,11 +1,11 @@
 from code_challenges.stack_and_queue.queue import Queue
 
-def breadth_first(root):
+def breadth_first(tree):
     final_list = []
     new_que = Queue()
-    new_que.enqueue(root)
+    new_que.enqueue(tree.root)
 
-    while new_que.isempty() != True:
+    while new_que.isempty() == False:
         temp = new_que.dequeue()
         final_list.append(temp.value)
         if temp.left is not None:
@@ -13,6 +13,8 @@ def breadth_first(root):
         if temp.right is not None:
             new_que.enqueue(temp.right)
     return final_list
+
+
 
 
 
