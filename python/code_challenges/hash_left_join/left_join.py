ad29bfc910f18d09bcf5cde72bf405aed8cd2606
Chris Rarig <cwrarig20@gmail.com>
2022-03-22 22:22:19 -0500

    updated code challenge tests

--- python/code_challenges/hash_left_join/left_join.py
+++ python/code_challenges/hash_left_join/left_join.py
@@ -1,14 +1,16 @@
 from code_challenges.hash_table.hash_table import HashTable
 
 def left_join(hash1, hash2):
-    key = HashTable.keys()
     new_list = []
-    keys1 = hash1.key
+    keys1 = HashTable.keys(hash1)
+    keys2 = HashTable.keys(hash2)
     for i in keys1:
         if i in keys2:
-            new_list.append(hash1[i])
-            new_list.append(hash2[i])
+            new_list.append(i)
+            new_list.append(HashTable.get(hash1,i))
+            new_list.append(HashTable.get(hash2,i))
         else:
-            new_list.append(hash1[i])
+            new_list.append(i)
+            new_list.append(HashTable.get(hash1,i))
             new_list.append('Null')
     return new_list
